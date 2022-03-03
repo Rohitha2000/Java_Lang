6d46ab79f6279338231175cedec41f65480848bc
Rohitha2000 <gourabathunirohitha1209@gmail.com>
2022-03-03 17:12:04 +0530

    evenorodd.java added

---
+++ com/java/FirstJava/EvenorOdd.java
@@ -0,0 +1,15 @@
+package com.java.FirstJava;
+
+import java.util.Scanner;
+
+public class EvenorOdd {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter 1st number");  // entering number
+        int num1= input.nextInt();
+        System.out.println("Enter 2nd number");
+        int num2= input.nextInt();
+        int result= (num1> num2)? num1 : num2; // using ternary operator
+        System.out.println("Largest number : " +result);
+    }
+}
---
+++ com/java/FirstJava/Main.java
@@ -0,0 +1,16 @@
+package com.java.FirstJava;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("JAVA DSA");
+        System.out.println("JAVA DSA");
+        System.out.println("JAVA DSA");
+        Scanner ss= new Scanner(System.in);
+        int num= ss.nextInt();
+        System.out.println(num);
+    }
+
+}
