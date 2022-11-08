ddabd2785500507721d5bfb71fdeaec22c4eea08
rohitha <gourabathunirohitha@gmail.com>
2022-11-09 03:59:46 +0530

    Heterogenous arrays

---
+++ com/java/Basics/HeterogenousArrays.java
@@ -0,0 +1,23 @@
+package com.java.Basics;
+
+public class HeterogenousArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Object[] heter= new Object[7];
+		heter[0]= new Integer(3);
+		heter[1]= new String("obj string1");
+		heter[2]= new Float(2.4454);
+		heter[3]= new Double(23.454545);
+		heter[4]= new Byte((byte) 23);
+		heter[5]= new Character('w');
+		heter[6]= new Boolean(true);
+		
+		for(Object h: heter) {
+			System.out.println(h);
+		}
+
+	}
+
+}
